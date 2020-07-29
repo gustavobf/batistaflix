675f4be4f59c3f88de1dc715fd4f236acffe3a28
Gustavo <gustavobatistafig@hotmail.com>
2020-07-29 14:09:54 -0300

    Estruturando cadastro de categoria

---
+++ src/components/FormField/index.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+
+function FormField({ label, type, name, value, onChange }) {
+    if (type === 'textarea') {
+        return (
+            <div>
+                <label>
+                    {label}:
+                <textarea
+                        type={type}
+                        value={value}
+                        name={name}
+                        onChange={onChange}
+                    />
+                </label>
+            </div>
+        )
+    }
+    return (
+        <div>
+            <label>
+                {label}:
+                <input
+                    type={type}
+                    value={value}
+                    name={name}
+                    onChange={onChange}
+                />
+            </label>
+        </div>
+    )
+}
+
+export default FormField;
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,26 +1,86 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+    const valoresIniciais = {
+        nome: '',
+        descricao: '',
+        cor: '#000000'
+    }
+    const [categorias, setCategoria] = useState([]);
+    const [values, setValues] = useState(valoresIniciais);
+
+    function setValue(chave, valor) {
+        setValues({
+            ...values,
+            [chave]: valor
+        })
+    }
+
+    function handleChange(infosDoEvento) {
+        setValue(
+            infosDoEvento.target.getAttribute('name'),
+            infosDoEvento.target.value
+        );
+    }
+
     return (
         <PageDefault>
-            <h1>Cadasto de Categoria</h1>
+            <h1>Cadasto de Categoria: {values.nome}</h1>
 
-            <form>
+            <form onSubmit={function handleSubmit(infosDoEvento) {
+                infosDoEvento.preventDefault();
 
-                <label>
-                    Nome da Categoria:
-                    <input
-                        type="text"
+                setCategoria([
+                    ...categorias,
+                    values
+                ]);
+
+                setValues(valoresIniciais);
+            }}>
+
+                <FormField
+                    label="Nome da Categoria"
+                    type="text"
+                    name="nome"
+                    value={values.nome}
+                    onChange={handleChange}
+                />
+
+                <FormField
+                    label="Descrição"
+                    type="textarea"
+                    name="descricao"
+                    value={values.descricao}
+                    onChange={handleChange}
+                />
+
+
+                <FormField
+                    label="Cor"
+                    type="color"
+                    name="cor"
+                    value={values.cor}
+                    onChange={handleChange}
                     />
-                </label>
 
                 <button>
                     Cadastrar
                 </button>
-                
+
             </form>
+
+            <ul>
+                {categorias.map((categoria, indice) => {
+                    return (
+                        <li key={`${categoria}${indice}`}>
+                            {categoria.nome}
+                        </li>
+                    )
+                })}
+            </ul>
 
             <Link to="/">
                 Ir para Home
